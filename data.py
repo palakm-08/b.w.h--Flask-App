f111316ed1a1cd0535bd8c8c855f0c52345ddb3f
hackathon08 <mishrapalak.886>
2026-01-11 22:31:06 +0530

    new page

---
+++ data.py
@@ -0,0 +1,23 @@
+from learn import app, db, products
+
+items = [
+    products(p_name='Lily', price=30, image='images/lily.jpg', desc='Elegant white bloom'),
+    products(p_name='Sunflower', price=100, image='images/sunflower.jpg', desc='Bright summer delight'),
+    products(p_name='Rose', price=20, image='images/rose.jpg', desc='Classic romantic flower'),
+    products(p_name='Daisy', price=50, image='images/daisy.jpg', desc='Simple cheerful bloom'),
+    products(p_name='Tulip', price=100, image='images/tulip.jpg', desc='Elegant springtime bloom'),
+    products(p_name='Iris', price=50, image='images/iris.jpg', desc='Graceful flower with rich symbolism'),
+    products(p_name='Freesia', price=80, image='images/freesia.jpg', desc='Sweet-scented delicate flower'),
+    products(p_name='Peony', price=200, image='images/peony.jpg', desc='Lush and luxurious blossom'),
+    products(p_name='Gerbera', price=75, image='images/gerbera.jpg', desc='Bright and cheerful daisy flower'),
+    products(p_name='Orchid', price=100, image='images/orchid.jpg', desc='Exotic and sophisticated bloom'),
+    products(p_name='Carnation', price=200, image='images/carnation.jpg', desc='Long-lasting ruffled flower'),
+    products(p_name='Lavender', price=40, image='images/lavender.jpg', desc='Fragrant calming purple flower'),
+    products(p_name='Chrysanthemum', price=100, image='images/chrysanthemum.jpg', desc='Classic full-bodied flower'),
+    products(p_name='Plumeria', price=50, image='images/plumeria.jpg', desc='Soft tropical fragrant bloom'),
+    products(p_name='Hibiscus', price=508, image='images/hibiscus.jpg', desc='Tropical bold vibrant flower')
+]
+
+with app.app_context():
+    db.session.add_all(items)
+    db.session.commit()
--- learn.py
+++ learn.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:palak_1008@localhost:5432/flask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:palak_1008@localhost:5432/flower'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -9,6 +9,7 @@ class products(db.Model):
     p_id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
     p_name = db.Column(db.String(200), nullable=False)
     price = db.Column(db.Numeric(10,2), nullable=False)
+    image = db.Column(db.String(300), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
 
 with app.app_context():
@@ -25,24 +26,8 @@ def about_pg(username):
 
 @app.route('/shop')
 def shop_products():
-    items = [
-        {'p_id' : 1, 'p_name' : 'Lily', 'price' : 30, 'image' : 'lily.jpg', 'desc' : 'Elegant white bloom'},
-        {'p_id' : 2, 'p_name' : 'Sunflower', 'price' : 100, 'image' : 'sunflower.jpg', 'desc' : 'Bright summer delight'},
-        {'p_id' : 3, 'p_name' : 'Rose', 'price' : 20, 'image' : 'rose.jpg', 'desc' : 'Classic romantic flower'},
-        {'p_id' : 4, 'p_name' : 'Daisy', 'price' : 50, 'image' : 'daisy.jpg', 'desc' : 'Simple cheerful bloom'}
-        # {'p_id' : 5, 'p_name' : 'Tullip', 'price' : 100},
-        # {'p_id' : 6, 'p_name' : 'Iris', 'price' : 50},
-        # {'p_id' : 7, 'p_name' : 'Freesia', 'price' : 80},
-        # {'p_id' : 8, 'p_name' : 'Peony', 'price' : 200},
-        # {'p_id' : 9, 'p_name' : 'Gerbera', 'price' : 75},
-        # {'p_id' : 10, 'p_name' : 'Orchid', 'price' : 100},
-        # {'p_id' : 11, 'p_name' : 'Carnation', 'price' : 200},
-        # {'p_id' : 12, 'p_name' : 'Lavender', 'price' : 40},
-        # {'p_id' : 13, 'p_name' : 'Chrysanthemum', 'price' : 100},
-        # {'p_id' : 14, 'p_name' : 'Plumeria', 'price' : 50},
-        # {'p_id' : 15, 'p_name' : 'Hibiscus', 'price' : 508},
-    ]
-    return render_template('shop.html', items=items)
+    all_products= products.query.all()
+    return render_template('shop.html', items=all_products)
 
 
 
