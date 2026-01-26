001c53efd2d1801312ea4f12259de32c07d11b2f
palakm-08 <mishrapalak.886@gmail.com>
2026-01-26 22:46:04 +0530

    user_validation

--- flower/routes.py
+++ flower/routes.py
@@ -1,16 +1,27 @@
 from flower import app
-from flask import render_template
-from flower.models import products
+from flask import render_template, redirect, url_for
+from flower.models import products, User
 from flower.forms import RegisterForm
+from flower import db
 
 @app.route('/')
 @app.route('/home')
 def home_pg():
     return render_template('home.html')
 
-@app.route('/profile/register')
+@app.route('/profile/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def create_account():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(
+            u_name = form.username.data,
+            u_email = form.user_email.data,
+            password_hash = form.pass1.data
+            )
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('shop_products'))
     return render_template('regForm.html', form=form)
 
 @app.route('/shop')
