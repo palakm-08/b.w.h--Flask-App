7af162b0ba5de906a798dbcb95433ce2c71e7e28
palakm-08 <mishrapalak.886@gmail.com>
2026-01-26 23:06:03 +0530

    validations

--- flower/forms.py
+++ flower/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User Name')
-    user_email = StringField(label='Email')
-    pass1 = PasswordField(label='Password')
-    pass2 = PasswordField(label='Confirm Password')
+    username = StringField(label='User Name', validators=[Length(min=4, max=16), DataRequired()])
+    user_email = StringField(label='Email', validators=[Email(), DataRequired()])
+    pass1 = PasswordField(label='Password', validators=[Length(min=8, max=15), DataRequired()])
+    pass2 = PasswordField(label='Confirm Password', validators=[EqualTo('pass1'), DataRequired()])
     submit_btn = SubmitField(label='Create Account')
